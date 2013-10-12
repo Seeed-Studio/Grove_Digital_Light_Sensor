90c761a3ffbbaca0d9a5705bdfd7044261b5fa09
elefantasy <zhangkun@seeedstudio.com>
2013-10-12 16:21:55 +0800

    Update Digital_Light_Sensor.ino
    delect the code that was commented note.

--- examples/Digital_Light_Sensor/Digital_Light_Sensor.ino
+++ examples/Digital_Light_Sensor/Digital_Light_Sensor.ino
@@ -13,26 +13,11 @@ void setup()
 }
 
 void loop()
-{  
-    // int CH0_LOW,CH0_HIGH,CH1_LOW,CH1_HIGH;
-    // int Channal0,Channal1;
+{   
     unsigned long  Lux;
-    //read two bytes from registers 0x0C and 0x0D
-    //  CH0_LOW=TSL2561.readRegister(TSL2561_ADDRESS,TSL2561_CHANNAL0L);
-    //  CH0_HIGH=TSL2561.readRegister(TSL2561_ADDRESS,TSL2561_CHANNAL0H);
-    //  CH1_LOW=TSL2561.readRegister(TSL2561_ADDRESS,TSL2561_CHANNAL1L);
-    //  CH1_HIGH=TSL2561.readRegister(TSL2561_ADDRESS,TSL2561_CHANNAL1H);
-    //  
-    //  Channal0=CH0_HIGH*256+CH0_LOW;
-    //  Channal1=CH1_HIGH*256+CH1_LOW;
-    //    
-    //  Serial.print("Channal0:");
-    //  Serial.println( Channal0);
-    //  Serial.print( "Channal1:");
-    //  Serial.println( Channal1);
-    TSL2561.getLux();
-  //  Lux=TSL2561.calculateLux(0,0,1);
     
+    TSL2561.getLux();
+ 
     Serial.print("The Light value is: ");
     Serial.println(TSL2561.calculateLux(0,0,1));
     delay(1000);
